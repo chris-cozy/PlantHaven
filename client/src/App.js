0548ff72167e8672fd105f56484f9b6f04668d9b
chris-cozy <csande9@clemson.edu>
2023-10-09 21:43:08 -0400

    Included authorization into navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Navigation, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigation,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
@@ -12,6 +18,7 @@ function App() {
   // Grab info from store
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -20,8 +27,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
