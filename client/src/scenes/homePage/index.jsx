d1d102fa58ca4d7a097e0378a416176976378164
chris-cozy <csande9@clemson.edu>
2023-10-10 02:36:34 -0400

    Included posting box

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import NavBar from "../navbar";
 import { useSelector } from "react-redux";
 import UserWidget from "../widgets/UserWidget";
+import MyPostWidget from "../widgets/MyPostWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
@@ -14,7 +15,7 @@ const HomePage = () => {
                 <UserWidget userId={_id} picturePath={picturePath} />
             </Box>
             <Box flexBasis={isNonMobileScreens ? "42%": undefined} mt={isNonMobileScreens ? undefined: "2rem"}>
-                
+                <MyPostWidget picturePath={picturePath} />
             </Box>
             {isNonMobileScreens && (
                 <Box flexBasis="26%">
