33f4b2edaa7723e9555c7dee38627c3c79216e7a
chris-cozy <csande9@clemson.edu>
2023-10-10 00:09:26 -0400

    Updated home page to include necessary sections and user info

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,8 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import NavBar from "../navbar";
+import { useSelector } from "react-redux";
+import UserWidget from "../widgets/UserWidget";
 
 const HomePage = () => {
-    return (<Box><NavBar /></Box>)
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+    const {_id, picturePath} = useSelector((state) => state.user);
+    return (
+    <Box>
+        <NavBar />
+        <Box width="100%" padding="2rem 6%" display={isNonMobileScreens ? "flex": "block"} gap="0.5rem" justifyContent="space-between">
+            <Box flexBasis={isNonMobileScreens ? "26%": undefined}>
+                <UserWidget userId={_id} picturePath={picturePath} />
+            </Box>
+            <Box flexBasis={isNonMobileScreens ? "42%": undefined} mt={isNonMobileScreens ? undefined: "2rem"}>
+                
+            </Box>
+            {isNonMobileScreens && (
+                <Box flexBasis="26%">
+                
+                </Box>
+            )}
+        </Box>
+    </Box>
+    )
 }
 
 export default HomePage;
