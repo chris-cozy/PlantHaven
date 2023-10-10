15fc1759c4e67b46664762afdb0da3bc245146a9
chris-cozy <csande9@clemson.edu>
2023-10-10 12:09:40 -0400

    Implemented friends list widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import UserWidget from "../widgets/UserWidget";
 import MyPostWidget from "../widgets/MyPostWidget";
 import PostsWidget from "../widgets/PostsWidget";
 import AdvertWidget from "../widgets/AdvertWidget";
+import FriendListWidget from "../widgets/FriendListWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
@@ -24,6 +25,7 @@ const HomePage = () => {
                 <Box flexBasis="26%">
                     <AdvertWidget />
                     <Box m="2rem 0"> 
+                        <FriendListWidget userId={_id}/>
                     </Box>
                 </Box>
             )}
