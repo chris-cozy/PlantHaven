1129154629c6c21c3aa1d19f88738f72fc488e04
chris-cozy <csande9@clemson.edu>
2023-10-10 02:37:06 -0400

    Resolved dependency warnings

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -22,15 +22,17 @@ const UserWidget = ({userId, picturePath}) => {
     const medium = palette.neutral.medium;
     const main = palette.neutral.main;
 
-    const getUser = async() => {
-        const response = await fetch(`http://localhost:3001/users/${userId}`, {method: "GET", headers: {Authorization: `Bearer ${token}`}})
-        const data = await response.json();
-        setUser(data);
-    }
+    
 
     useEffect(() => {
+        const getUser = async() => {
+            const response = await fetch(`http://localhost:3001/users/${userId}`, {method: "GET", headers: {Authorization: `Bearer ${token}`}})
+            const data = await response.json();
+            setUser(data);
+        }
+
         getUser();
-    }, [])
+    }, [token, userId])
 
     if (!user){
         return null;
